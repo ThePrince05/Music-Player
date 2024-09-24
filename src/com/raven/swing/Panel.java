e63fd8432b4f5d1fc4656029065401135c93dcb8
Prince Sithole <princesithole49@gmail.com>
2024-09-24 08:09:58 +0200

    Inserted new package names.

---
+++ src/com/raven/swing/Panel.java
@@ -0,0 +1,26 @@
+
+package com.raven.swing;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+
+public class Panel extends JPanel{
+
+    public Panel() {
+        setOpaque(false);
+    }
+  
+    
+    @Override
+    protected void paintComponent(Graphics grphcs){
+        Graphics2D g2 = (Graphics2D)grphcs;
+        g2.setColor(getBackground());
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 30);
+        super.paintComponent(grphcs);
+        
+    }
+    
+}
